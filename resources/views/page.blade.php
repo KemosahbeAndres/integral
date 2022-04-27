3192ce9364315527245673759d0d1cfbe799cca3
Andres Cubillos <andrestj1996@gmail.com>
2022-04-26 21:02:24 -0400

    Vue in prod

--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Integral del Norte</title>
-    <script src="{{ asset('js/vue.js') }}"></script>
+    <script src="{{ asset('js/vue.min.js') }}"></script>
     <!-- <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}"> -->
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>

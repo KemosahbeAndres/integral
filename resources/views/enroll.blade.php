28a761bb615367406b95c7488c58a418d3685330
Andres Cubillos <andrestj1996@gmail.com>
2022-04-26 21:28:41 -0400

    Enroll form complete.

--- resources/views/enroll.blade.php
+++ resources/views/enroll.blade.php
@@ -6,14 +6,26 @@
         <form class="d-grid gap-3" action="{{ route('page.enroll.post') }}" method="post">
             <h1>Matriculate</h1>
             @csrf
-            <input class="form-control" type="text" name="name" placeholder="Nombres">
-            <input class="form-control" type="text" placeholder="Apellidos">
-            <input class="form-control" type="text" placeholder="RUT">
-            <input class="form-control" type="text" placeholder="correo">
-            <input class="form-control" type="text" placeholder="Telefono">
-            <input class="form-control" type="text" placeholder="Direccion">
+            <input class="form-control" type="text" name="name" placeholder="Nombres" required>
+            <input class="form-control" type="text" placeholder="Apellidos" required>
+            <input class="form-control" type="text" placeholder="Rut (sin puntos, con guion) Ejemplo: 11222333-k" required>
+            <input class="form-control" type="text" placeholder="Correo electronico" required>
+            <input class="form-control" type="text" placeholder="Telefono" required>
+            <input class="form-control" type="text" placeholder="Direccion" required>
+            <select class="form-select" name="situacion" aria-label="select" required>
+                <option selected>Situacion Laboral</option>
+                <option value="cesante">Cesante</option>
+                <option value="dependiente">Dependiente</option>
+                <option value="independiente">Independiente</option>
+            </select>
+            <select class="form-select" name="curso" required>
+                <option selected>Curso</option>
+                <option value="primerciclo">Primer Ciclo (1er y 2do año de Enseñanza Media)</option>
+                <option value="segundociclo">Segundo Ciclo (3er y 4to año de Enseñanza Media)</option>
+            </select>
             <input class="btn btn-lg btn-main btn-block" type="submit">
         </form>
     </div>
 </div>
 @endsection
+
